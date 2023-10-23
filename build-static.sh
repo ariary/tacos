7681bd73849cbba3888c2d05c3594d21ceda8981
ariary <ariary9.2@hotmail.fr>
2023-10-23 09:06:24 +0200

    ease target static build

--- build-static.sh
+++ build-static.sh
@@ -6,5 +6,11 @@ if [[ $# -ne 1 ]]; then
     exit 92
 fi
 
+EXENAME="tacos"
+TARGET=$(go tool dist list|gum filter --placeholder="choose target os & arch")
+
+export GOOS=$(echo $TARGET|cut -f1 -d '/')
+export GOARCH=$(echo $TARGET|cut -f2 -d '/')
+echo "build ${EXENAME} in ${PWD}"
 export REMOTE=$1
-CGO_ENABLED=0 go build -ldflags "-X 'main.Remote=${REMOTE}'" cmd/tacos/tacos.go
+CGO_ENABLED=0 go build -ldflags "-X 'main.Remote=${REMOTE}'" -o ${EXENAME} cmd/tacos/tacos.go 
